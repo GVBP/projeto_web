dc6b3107e6c5f7c1fb2fbf92ff706917a0885394
Guilherme <pereira.guilherme@totvs.com.br>
2019-10-29 00:58:47 -0300

    Implementação da Autenticação

--- app.js
+++ app.js
@@ -3,18 +3,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
 var postsRouter = require('./routes/posts');
 var projectsRouter = require('./routes/projects');
-var adminPostsRouter = require('./routes/admin/posts');
-var adminTestimonialsRouter = require('./routes/admin/testimonials');
-var adminProjectsRouter = require('./routes/admin/projects');
-var jobsRouter = require('./routes/jobs');
-var usersRouter = require('./routes/users');
 var testimonialsRouter = require('./routes/testimonials');
+var jobsRouter = require('./routes/jobs');
+var adminPostsRouter = require('./routes/admin/posts');
+var adminProjectsRouter = require('./routes/admin/projects');
+var adminTestimonialsRouter = require('./routes/admin/testimonials');
+var authRouter = require('./routes/auth');
+var verifyAuth = require('./middlewares/authMiddleware');
 
 var app = express();
+
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'mySecret123',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -27,14 +38,16 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/auth', authRouter);
+app.use('/admin/posts', [verifyAuth], adminPostsRouter);
+app.use('/users', usersRouter);
 app.use('/posts', postsRouter);
 app.use('/projects', projectsRouter);
 app.use('/testimonials', testimonialsRouter);
-app.use('/admin/posts', adminPostsRouter);
-app.use('/admin/testimonials', adminTestimonialsRouter);
-app.use('/admin/projects', adminProjectsRouter);
 app.use('/jobs', jobsRouter);
-app.use('/users', usersRouter);
+app.use('/admin/posts', adminPostsRouter);
+app.use('/admin/projects', adminProjectsRouter);
+app.use('/admin/testimonials', adminTestimonialsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
